a7ecd1bf627e9283e86a9e56a4ac4744e741cddd
JohnnyTheBoy <nikola.jandric@outlook.com>
2017-10-14 18:51:39 +0200

    Files created and connected

---
+++ main.js
@@ -0,0 +1,9 @@
+//#region - description
+// Kompjuter treba da generiše broj od 0 do 99. Korisnik ima 5 pokušaja da pogodi koji je broj u pitanju.
+// Ako pogodi broj, treba da dobije poruku tipa "Pobedio si", a ako pogreši treba da dobije poruku "Moj broj je veći"
+// ili "Moj broj je manji". Ako iskoristi svih 5 pokušaja treba da dobije poruku "Iskoristili ste svih 5 pokušaja,
+// traženi broj je bio 23".
+//#endregion
+
+
+
